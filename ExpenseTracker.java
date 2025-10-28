d1c7c0a36eb4313c6269d9d274a1895a695a1b51
babi-110905 <babi110905@gmail.com>
2025-10-28 07:44:02 +0530

    Add ExpenseTracker class to calculate total expenses

---
+++ ExpenseTracker.java
@@ -0,0 +1,6 @@
+public class ExpenseTracker {
+    public static void main(String[] args) {
+        double food = 1200.5, rent = 5000, total = food + rent;
+        System.out.println("Total expenses: " + total);
+    }
+}
